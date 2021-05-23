89d77fdd4ee4d17a0b61d8ccbe6c6f4bb345279e
o.unger <ounger@inbox.ru>
2021-05-23 17:58:34 +0300

    finish webpack config

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+module.exports = (env, argv) => {
+    const isProd = argv.mode === 'production'
+    const isDev = !isProd
+    const filename = (ext) => isProd
+        ? `[name].[contenthash].bundle.${ext}`
+        : `[name].bundle.${ext}`
+    const plugins = () => {
+        const base = [
+            new HtmlWebpackPlugin({
+                template: './index.html'
+            }),
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, 'src', 'favicon.ico'),
+                        to: path.resolve(__dirname, 'dist')
+                    },
+                ],
+            }),
+            new MiniCssExtractPlugin({
+                filename: filename('css')
+            }),
+        ]
+        if (isDev) base.push(new ESLintPlugin())
+        return base
+    }
+    return {
+        context: path.resolve(__dirname, 'src'),
+        entry: {
+            main: ['@babel/polyfill', './index.js', ]
+        },
+        resolve: {
+            //'../path/file'
+            extensions: ['.js'],
+            //  вместо '../../../path/file'
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@core': path.resolve(__dirname, 'src', 'core')
+            }
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: filename('js'),
+            clean: true, //Cleaning up the /dist folder
+        },
+        devtool: isDev ? 'source-map' : false,
+        plugins: plugins(),
+        module: {
+            rules: [
+                //sass
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        MiniCssExtractPlugin.loader,
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                    ],
+                },
+                //babel
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ],
+        },
+        devServer: {
+            open: true,
+            watchContentBase: true // автоперезагрузка вместе кроме html
+            // contentBase: path.join(__dirname, 'dist'),
+            // compress: true,
+            //port: 9000, //или в package.json
+            // hot: true, // автоперезагрузка, кроме html
+        },
+        target: 'web' // без этго не работает автоперезагрузка страницы
+    }
+}
+
+
